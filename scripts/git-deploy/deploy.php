5144433e85863ff2f77e61a4d0df06764829e884
Norman Kämper-Leymann <nleymann@intrax.de>
2017-05-12 16:02:25 +0200

    Update deploy script

--- scripts/git-deploy/deploy.php
+++ scripts/git-deploy/deploy.php
@@ -93,3 +93,23 @@ if (!empty(TOKEN) && isset($_SERVER["HTTP_X_HUB_SIGNATURE"]) && $token !== hash_
 
 fputs($file, "\n\n" . PHP_EOL);
 fclose($file);
+// Make it pretty for manual user access (and why not?)
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
