3b2a62b05bb98415104990f6680d5d2c334361a7
Norman Kämper-Leymann <norman.leymann@gmail.com>
2018-07-09 11:26:03 +0200

    Add sample script.

---
+++ scripts/deployment/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Set script to exit on errors.
+set -e
+
+# Get script's absolute location.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change to repo root.
+cd ${DIR};
+cd ../..
+
+# Git pull, composer install.
+git pull
+composer install --no-interaction --no-dev --prefer-dist
+
+# Drupal routine.
+cd web/
+../bin/drush state:set system.maintenance_mode 1 -y
+../bin/drush cache:rebuild -y
+../bin/drush updatedb -y
+../bin/drush config:import -y
+../bin/drush entity:updates -y
+../bin/drush core:cron -y
+../bin/drush state:set system.maintenance_mode 0 -y
+../bin/drush cache:rebuild -y
